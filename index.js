98c3a329e7eb3cfa22f2408da6c760b18ef5f2ba
Andrew Martins <andrew_omartins@yahoo.com>
2022-06-07 03:53:08 -0400

    added logic for empty inputs

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const todoObjectList = []
 
 class Todo_Class {
     constructor(item){
+        console.log(item)
         this.ulElement = item
     }
 
     add() {
-
+        const todoInput = document.querySelector("myInput").value
+        if(todoInput == ""){
+            alert("Dont make me fight you..")
+        }else {
+            const todoObject = {
+                id: todoObjectList.length,
+                todoText: todoInput,
+                isDone: false,
+            }
+            todoObjectList.unshift(todoObject)
+            this.display()
+            document.querySelector("#task").value = ''
+        }
     }
 
     done_undone(x) {
@@ -27,3 +40,7 @@ class Todo_Class {
 const listSection = document.querySelector('#myUL')
 
 myTodoList = new Todo_Class(listSection)
+
+document.querySelector(".addBtn").addEventListener("click", function() {
+    myTodoList.add()
+})
