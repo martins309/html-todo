f999efb7cacc87953693f163d03fe7357c1148d2
Andrew Martins <andrew_omartins@yahoo.com>
2022-06-07 03:16:34 -0400

    adding javascript functionality

---
+++ index.js
@@ -0,0 +1,29 @@
+const todoObjectList = []
+
+class Todo_Class {
+    constructor(item){
+        this.ulElement = item
+    }
+
+    add() {
+
+    }
+
+    done_undone(x) {
+
+    }
+
+    deleteElement(z) {
+
+    }
+
+    display() {
+
+    }
+    
+}
+
+
+const listSection = document.querySelector('#myUL')
+
+myTodoList = new Todo_Class(listSection)
